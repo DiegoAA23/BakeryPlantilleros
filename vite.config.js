584d9943debcebe21fb7cd68a29ca8692e5ccb2b
Diego Alvarado <diegoale2304@outlook.com>
2023-11-10 15:55:30 -0600

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/examen-segundo-parcial/",
+    base: "/BakeryPlantilleros/",
     appType: 'mpa',
     build: {
         rollupOptions: {
